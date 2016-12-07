17cc5da5cff6e94d3d76ea86cf03babe138f5f8f
Rafael Jesus <rafaelljesus86@gmail.com>
2016-12-07 00:04:11 -0200

    Litle code update

--- server.js
+++ server.js
@@ -1,22 +1,21 @@
-'use strict';
+'use strict'
 
-var fs              = require('fs')
-  , split           = require('split')
-  , request         = require('request')
-  , ParallelStream  = require('./lib/parallel-stream');
+const fs = require('fs')
+const split = require('split')
+const request = require('request')
+const ParallelStream = require('./lib/parallel-stream')
 
 fs.createReadStream(process.argv[2])
   .pipe(split())
-  .pipe(new ParallelStream(function(url, enc, cb) {
-    if (!url) return cb();
-    var self = this;
-    request.head(url, function(err, res) {
-      var text = url + ' is ' + (err ? 'down' : 'up' + '\n');
-      self.push(text);
-      cb();
-    });
+  .pipe(new ParallelStream(function (url, enc, cb) {
+    if (!url) return cb()
+    request.head(url, (err, res) => {
+      const text = url + ' is ' + (err ? 'down' : 'up' + '\n')
+      console.log(text)
+      this.push(text)
+      cb()
+    })
   }))
   .pipe(fs.createWriteStream('status.txt'))
-  .on('finish', function() {
-    console.log('All urls where checked');
-  });
+  .on('finish', () =>
+    console.log('All urls where checked'))
